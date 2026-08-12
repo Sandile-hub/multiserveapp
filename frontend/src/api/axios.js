9a39b73019d62409f8d2da82a02a2a4a5e7ad780
Sandile Magagula <sandiledr100@gmail.com>
2026-08-12 15:51:34 +0200

    Fix API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,28 +1,26 @@
-import axios from "axios"
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
-})
+  withCredentials: true,
+});
 
 API.interceptors.request.use(
   (config) => {
-
-    const token =
-      localStorage.getItem("token")
+    const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
-    return config
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default API
+export default API;
